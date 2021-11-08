3ddc86b688ba6c90b377ea3f6885e69ee01ae860
malki <malki.attal@hotmail.fr>
2021-11-08 16:55:37 +0200

    change the root of the webhook

--- devOps/application.py
+++ devOps/application.py
@@ -4,7 +4,7 @@ app =Flask(__name__)
 
 print("blablabla")
 
-@app.route('/github-webhook/', methods=['POST']) 
+@app.route('/', methods=['POST']) 
 def hello_world(): 
     print("ive got something")
     print(request.json) 
