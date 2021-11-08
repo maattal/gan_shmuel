dc949dbe388bbad719f01cb0baa511153ceec4dc
malki <malki.attal@hotmail.fr>
2021-11-08 17:19:52 +0200

    webhook 404

--- devOps/application.py
+++ devOps/application.py
@@ -9,5 +9,7 @@ def hello_world():
     print("ive got something")
     print(request.json) 
     return ("this is webhook", 200, None)
+ 
     
-if __name__== '__main__': app.run(host="0.0.0.0",debug=True) 
+if __name__== '__main__': 
+    app.run(host="0.0.0.0",debug=True) 
