cb402c3caf09ba10fb6083b8257b599e60d5285a
malki <malki.attal@hotmail.fr>
2021-11-08 18:05:12 +0200

    parse the json data into dictionary

--- devOps/application.py
+++ devOps/application.py
@@ -1,4 +1,5 @@
 from flask import Flask,request
+from flask.wrappers import JSONMixin
 # #https://github.com/maattal/gan_shmuel.git 
 app =Flask(__name__) 
 
@@ -9,6 +10,8 @@ def home():
 @app.route('/', methods=['POST']) 
 def post_request(): 
     print(request.json) 
+    dict_data=JSONMixin.loads(request.json)
+    print(dict_data)
     return ("this is a request post from github webhook", 200, None)
 
 
