efaaf9a4e1c4e58a58feea1ea4398212150321ed
EC2 Default User <ec2-user@ip-172-31-33-69.eu-central-1.compute.internal>
2021-11-08 13:25:23 +0000

    webhook config

--- devOps/application.py
+++ devOps/application.py
@@ -1,4 +1,8 @@
-from flask import Flask
-
-
-print("hello-world!")
+from flask import Flask,request
+#https://github.com/maattal/gan_shmuel.git 
+app =Flask(__name__) 
+@app.route('/github.com/maattal/gan_shmuel.git', methods=['POST']) 
+def hello_world(): 
+    print(request.json) 
+    return "SOMTING IS ALIVE!" 
+if __name__== '__main__': app.run(host="0.0.0.0",debug=True) 
