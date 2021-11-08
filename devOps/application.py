1426f2a6e8f37aa8d85e361db6eefd57a521178f
malki <malki.attal@hotmail.fr>
2021-11-08 16:48:14 +0200

    change the root of the first webhook

--- devOps/application.py
+++ devOps/application.py
@@ -4,8 +4,9 @@ app =Flask(__name__)
 
 print("blablabla")
 
-@app.route('/github.com/maattal/gan_shmuel.git', methods=['POST']) 
+@app.route('/github-webhook/', methods=['POST']) 
 def hello_world(): 
     print(request.json) 
     return "SOMTING IS ALIVE!" 
+    
 if __name__== '__main__': app.run(host="0.0.0.0",debug=True) 
