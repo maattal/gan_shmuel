abbba13d3d6aae1d79a4e55ba599b8ec0875104c
malki <malki.attal@hotmail.fr>
2021-11-10 14:22:47 +0200

    update application.py

--- devOps/application.py
+++ devOps/application.py
@@ -45,14 +45,4 @@ if __name__== '__main__':
     app.run(host="0.0.0.0",debug=True,port='8085') 
 
 
-    
-
-#--------------------BUILD OF THE CONTAINERS------------
-
-#
-
-
-
-
-if _name== 'main_': 
-    app.run(host="0.0.0.0",debug=True,port='8085')
+  
